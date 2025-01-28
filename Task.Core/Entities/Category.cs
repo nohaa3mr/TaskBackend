d7ea48323bb4a983834f4445c495c29ceabc3b2f
noha <kookygamal1@gmail.com>
2025-01-28 13:08:29 +0200

    Add project files.

---
+++ Task.Core/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Core.Entities
+{
+    public class Category
+    { //(BookId, Name, Description, Price, Auther , stock)
+
+    }
+}
